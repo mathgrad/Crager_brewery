e8d214750faa20561a01bd4a93c2dbe057a3a9fc
Joshua Crager <jscrager@gmail.com>
2022-11-04 14:20:18 -0500

    Update sliceBrewery.js
    Removed logging

--- src/redux/sliceBrewery.js
+++ src/redux/sliceBrewery.js
@@ -3,9 +3,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import * as api from "./api/brewery";
 
 export const searchCity = createAsyncThunk("search/city", async (props) => {
-  console.log("Hit function");
   return await api.ByCity(props).then((data) => {
-    console.log(data);
     return data;
   });
 });
@@ -20,19 +18,14 @@ const brewerySlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(searchCity.pending, (state) => {
-        console.log("Search By City Started");
-      })
+      .addCase(searchCity.pending, (state) => {})
       .addCase(searchCity.fulfilled, (state, action) => {
         state.list = action.payload;
-        console.log("Search By City Ended");
       });
   },
 });
 
-// export const { fake } = brewerySlice.actions;
 export const breweryList = (state) => {
-  console.log(state);
   return state.brewery.list;
 };
 export default brewerySlice.reducer;
