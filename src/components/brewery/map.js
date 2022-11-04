2b498b33ba64f62de5cffeda331503c028fdfe35
Joshua Crager <jscrager@gmail.com>
2022-11-04 16:37:43 -0500

    Update map.js
    Added in an error check

--- src/components/brewery/map.js
+++ src/components/brewery/map.js
@@ -3,27 +3,27 @@ import { osm } from "pigeon-maps/providers";
 
 const MyMap = (props) => {
   props = props.props;
-  console.log(
-    "props coordinates:",
-    props.latitude,
-    props.longitude,
-    "\nconverted: ",
-    parseFloat(props.latitude),
-    parseFloat(props.longitude)
-  );
+
   const coordinate = {
-    lat: parseFloat(props.latitude) || 0,
-    long: parseFloat(props.longitude) || 0,
+    lat: parseFloat(props.latitude),
+    long: parseFloat(props.longitude),
   };
+
   return (
-    <Map
-      provider={osm}
-      height={300}
-      defaultCenter={[coordinate.lat, coordinate.long]}
-      defaultZoom={11}
-    >
-      <Marker width={50} anchor={[coordinate.lat, coordinate.long]} />
-    </Map>
+    <>
+      {coordinate.lat !== null || coordinate.lat !== NaN ? (
+        <Map
+          provider={osm}
+          height={300}
+          defaultCenter={[coordinate.lat, coordinate.long]}
+          defaultZoom={11}
+        >
+          <Marker width={50} anchor={[coordinate.lat, coordinate.long]} />
+        </Map>
+      ) : (
+        <div>Bad location</div>
+      )}
+    </>
   );
 };
 
